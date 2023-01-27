fe066c17f3f341922641546f3a226aa1ed14dda8
Tirth Shah <tirthshah147@gmail.com>
2023-01-27 15:45:10 +0530

    Done

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+// Import routes
+
+//Router Middlewares
+app.use(express.json());
+// app.use('/api/user', authRoute);
+
+module.exports = app;
